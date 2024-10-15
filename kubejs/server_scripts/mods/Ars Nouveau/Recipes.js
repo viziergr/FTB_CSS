cca1c278604e0af073a93b4bef332394a1549dfc
viziergr <gregoire.vizier@reseau.eseo.fr>
2024-10-15 17:20:35 +0200

    Add initial configuration files

---
+++ kubejs/server_scripts/mods/Ars Nouveau/Recipes.js
@@ -0,0 +1,20 @@
+ServerEvents.recipes(event => {
+    // event.recipes.ars_nouveau.imbuement(
+    //     "minecraft:sand", // input item
+    //     "minecraft:tnt", // output
+    //     1000, // source cost
+    //     []
+    // )
+    // event.recipes.ars_nouveau.enchanting_apparatus(
+    //     [
+    //         "minecraft:sand",
+    //         "minecraft:sand",
+    //         "minecraft:sand",
+    //         "minecraft:sand",
+    //     ], // input items
+	//     "minecraft:gunpowder", // reagent
+	//     "minecraft:tnt", // output
+	//     1000, // source cost
+	//     // true // keep nbt of reagent, think like a smithing recipe
+	// );
+})
---
+++ kubejs/server_scripts/mods/CSS/Recipes.js
@@ -0,0 +1,19 @@
+ServerEvents.recipes(event => {
+
+    ////////////////////////////////////////////
+    //                                        //
+    //                   CSS                  //
+    //                                        //
+    ////////////////////////////////////////////
+
+    // Storage Upgrade (I)
+
+    event.shaped(
+        Item.of('css:soleil', 1,), ['DAD', 'ACA', 'DAD'], {
+            A: 'mekanism:pellet_antimatter',
+            C: 'mekanism:supercharged_coil',
+            D: 'mekanism:reprocessed_fissile_fragment'
+        }
+    )
+    }
+)
